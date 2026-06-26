b364348fc59610113cde5459e2fbaf8b33351098
iambb5445 <bbateni@ucsc.edu>
2026-06-25 22:05:55 -0700

    Use utf-8 encoding for saving files since skill.md might contain special characters

--- util.py
+++ util.py
@@ -7,19 +7,19 @@ def _get_filepath(dir: str|list[str], filename: str) -> str:
     return Path(path).as_posix()
 
 def read_file(dir: str|list[str], filename: str) -> str:
-    with open(_get_filepath(dir, filename), "r") as f:
+    with open(_get_filepath(dir, filename), "r", encoding="utf-8") as f:
         content = f.read()
     return content
 
 def write_file(dir: str|list[str], filename: str, content: str) -> None:
-    with open(_get_filepath(dir, filename), "w") as f:
+    with open(_get_filepath(dir, filename), "w", encoding="utf-8") as f:
         f.write(content)
 
 def read_dict(dir: str|list[str], filename: str) -> dict[str, str]:
-    with open(_get_filepath(dir, filename), "r") as f:
+    with open(_get_filepath(dir, filename), "r", encoding="utf-8") as f:
         content = json.load(f)
     return content
 
 def write_dict(dir: str|list[str], filename: str, content: dict|list) -> None:
-    with open(_get_filepath(dir, filename), "w") as f:
+    with open(_get_filepath(dir, filename), "w", encoding="utf-8") as f:
         json.dump(content, f)
